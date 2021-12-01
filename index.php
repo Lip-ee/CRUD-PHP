8e11df75107fed8afd73229e15ccf30f38b7759c
Filipe Campos <63020408+Lip-ee@users.noreply.github.com>
2021-12-01 01:03:42 -0300

    LJ-php-crud
    no-folder files

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
+
+<?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>
+
+
+<h1> <img src="https://cdn-icons-png.flaticon.com/512/3884/3884609.png" width="45"> Dashboard</h1>
+<hr />
+
+<?php if ($db) : ?>
+
+<div class="row">
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers/add.php" class="btn btn-primary">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-plus fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Novo Cliente</p>
+				</div>
+			</div>
+		</a>
+	</div>
+
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers" class="btn btn-default">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Clientes</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
+
+<?php else : ?>
+	<div class="alert alert-danger" role="alert">
+		<p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
+	</div>
+
+<?php endif; ?>
+
+<?php include(FOOTER_TEMPLATE); ?>
